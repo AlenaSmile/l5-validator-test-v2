321c89c539d46f817ee58a55a16a83aada55f296
AlenaSmile <alenaeliseenko2003@gmail.com>
2023-10-30 15:30:55 +0300

    src/ArraySchema.js src/StringSchema.js src/Validator.js

--- src/Validator.js
+++ src/Validator.js
@@ -1,1 +1,15 @@
+/* eslint-disable class-methods-use-this */
+import StringSchema from './StringSchema.js';
+import ArraySchema from './ArraySchema.js';
 
+export default class Validator {
+  string() {
+    const validator = (value) => typeof value === 'string';
+    return new StringSchema([validator]);
+  }
+
+  array() {
+    const validator = (value) => Array.isArray(value);
+    return new ArraySchema([validator]);
+  }
+}
